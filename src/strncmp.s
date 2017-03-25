2311d2d65c34318520e8c204264fedf382f8ed3d
Maxence DELACROIX <maxence@debian>
2017-03-25 16:38:28 +0100

    little condition on counter

--- src/strncmp.s
+++ src/strncmp.s
@@ -6,22 +6,24 @@ global strncmp:function
 
 strncmp:
 	xor	rax, rax	; set rax at the 0 value
+	xor	r8, r8
 
 try:
-	mov	al, byte[rsi]	; save the byte pointed by rsi in al
-	mov	r10b, byte[rdi]	; save the byte pointed by rdi in r10b
-	cmp	al, r10b	; compare the value of al and r10b
-	jne	return		; (if != 0 jump to return)
-	cmp	r10b, 0		; compare the value of al and 0
-	je	return		; (if == 0 jump to return)
-	dec	rdx		; decrement the value of the third argument
 	cmp	rdx, 0
 	je	return
-	inc	rsi		; increment the value of esi (rsi + 1)
-	inc	rdi		; increment the value of edi (rdi + 1)
+	mov	al, byte[rsi + r8]	; save the byte pointed by rsi in al
+	mov	r10b, byte[rdi + r8]	; save the byte pointed by rdi in r10b
+	inc	r8
+	cmp	al, r10b		; compare the value of al and r10b
+	jne	return			; (if != 0 jump to return)
+	cmp	r10b, 0			; compare the value of al and 0
+	je	return			; (if == 0 jump to return)
+	dec	rdx			; decrement the value of the third argument
 	jmp	try
 
 return:
+	cmp	r8, 0
+	je	exit
 	sub	r10b, al	; substract the value r10b by al
 	movsx	rax, r10b	; set the return value to the value of the substract
 
